f07efd3d756e2f9643135945acd908d0e59840e2
Omar Rahman <omar@r4men.com>
2026-06-28 16:47:48 -0700

    Testing another fen

--- src/main/java/com/r4men/java_chess/JavaChess.java
+++ src/main/java/com/r4men/java_chess/JavaChess.java
@@ -2,7 +2,8 @@ package com.r4men.java_chess;
 
 public class JavaChess {
     void main(String[] args) {
-        Board board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+//        Board board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        Board board = new Board("8/8/5KQk/8/8/8/8/8 b - - 27 70");
 
         System.out.println(board);
     }
