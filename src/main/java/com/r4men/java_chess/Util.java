9c8c8c5b759df8ffdd077c65f82c1847e14fd672
Omar Rahman <omar@r4men.com>
2026-07-05 08:22:10 -0700

    Update Util.java

--- src/main/java/com/r4men/java_chess/Util.java
+++ src/main/java/com/r4men/java_chess/Util.java
@@ -88,6 +88,7 @@ public class Util {
     // 2 | 08 09 10 11 12 13 14 15 | 2
     // 1 | 00 01 02 03 04 05 06 07 | 1
     //      A  B  C  D  E  F  G  H
+
     public static int convert8x8to10x12(int s8x8) {
         if (s8x8 < 0 || s8x8 >= 64) {
             throw new  IllegalArgumentException("Invalid 8x8 square (Off-Board on 8x8): " + s8x8);
