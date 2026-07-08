5b602ae05edf0ebdcff13adae351ab28daae5797
Omar Rahman <omar@r4men.com>
2026-07-07 19:26:17 -0700

    Update JavaChess.java

--- src/main/java/com/r4men/java_chess/JavaChess.java
+++ src/main/java/com/r4men/java_chess/JavaChess.java
@@ -2,14 +2,23 @@ package com.r4men.java_chess;
 
 public class JavaChess {
     void main(String[] args) {
-        Board board = new Board("rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3");
+        Board board = new Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
+        board.makeMove("e2e4");
+        board.makeMove("f7f5");
+        board.makeMove("e4e5");
+        board.makeMove("d7d5");
         board.makeMove("e5d6");
         board.makeMove("f5f4");
         board.makeMove("g2g4");
         board.makeMove("f4g3");
         board.makeMove("d1f3");
+        board.makeMove("g3f2");
+        board.makeMove("d2d4");
+        board.makeMove("f3f2");
+        board.makeMove("d8d6");
+        board.makeMove("g1f3");
 
-        System.out.println(board);
+        System.out.println(board.ascii8x8());
     }
 }
